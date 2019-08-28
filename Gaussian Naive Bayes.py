9e14f977513ee1abdc9fbe531547be56b85d933b
H-Y-H-Y-H <48082207+H-Y-H-Y-H@users.noreply.github.com>
2019-08-27 22:21:19 -0400

    Update Gaussian Naive Bayes.py

--- Gaussian Naive Bayes.py
+++ Gaussian Naive Bayes.py
@@ -1,21 +1,17 @@
 >>> import numpy as np
 
 >>> #生成可利用的训练点
->>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
->>> Y = np.array([1, 1, 1, 2, 2, 2])
+>>> Feature = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+>>> Labels = np.array([1, 1, 1, 2, 2, 2])
 
 >>> from sklearn.naive_bayes import GaussianNB #调用外部函数
 #创建分类器 Classifier
 >>> clf = GaussianNB()
->>> clf.fit(X, Y) #fit 拟合，也就是train 训练函数，x为feature，y为labels
+>>> clf.fit(Feature, Labels) #fit 拟合，也就是train 训练函数，x为feature，y为labels
 GaussianNB(priors=None, var_smoothing=1e-09)
 
 #让分类器做预测
 >>> print(clf.predict([[-0.8, -1]]))
+#分类器显示是属于第一类
 [1]
 
-
->>> clf_pf = GaussianNB()
->>> clf_pf.partial_fit(X, Y, np.unique(Y))
-GaussianNB(priors=None, var_smoothing=1e-09)
->>> print(clf_pf.predict([[-0.8, -1]]))
